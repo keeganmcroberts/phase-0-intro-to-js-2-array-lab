6183f76501a18f327aa3cec7cf8f03945008f912
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-10 14:31:37 -0500

    commit tree 1bbc5f117273596d783b0cdbc4233a7f61824ccb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    let firstCat = [...cats, 'Broom']
+    return firstCat;
+}
+function prependCat(){
+    let lastCat = ["Arnold", ...cats]
+    return lastCat;
+}
+function removeLastCat(){
+    let newCat = cats.slice(0,2)
+    return newCat;
+}
+function removeFirstCat(){
+    let aCat = cats.slice(1)
+    return aCat;
+}
